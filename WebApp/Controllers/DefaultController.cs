ee06ef18094162855040abc6e3fa2833d2f43506
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-13 02:52:56 +0300

    Add user layout and view for blogs

---
+++ WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
