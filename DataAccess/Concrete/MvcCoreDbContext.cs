c9d35b8140c219c0e942e04a0c0b2a7fe855f560
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-13 00:52:15 +0300

    Add context and first migration

---
+++ DataAccess/Concrete/MvcCoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class MvcCoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=MvcCore;Trusted_Connection=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
