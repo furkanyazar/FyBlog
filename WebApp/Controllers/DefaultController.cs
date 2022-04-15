912695756de7be2ee228728e1d67c5d92b076233
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-15 18:44:18 +0300

    Update a tags

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -3,53 +3,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
-    public class DefaultController : Controller
-    {
-        private ICommentService _commentService;
+	public class DefaultController : Controller
+	{
+		public PartialViewResult HeaderSearchPartial()
+		{
+			return PartialView();
+		}
 
-        public DefaultController(ICommentService commentService)
-        {
-            _commentService = commentService;
-        }
+		public PartialViewResult FooterAboutUsPartial()
+		{
+			return PartialView();
+		}
 
-        public PartialViewResult HeaderSearchPartial()
-        {
-            return PartialView();
-        }
+		public PartialViewResult FooterSignupNewsletterPartial()
+		{
+			return PartialView();
+		}
 
-        public PartialViewResult FooterAboutUsPartial()
-        {
-            return PartialView();
-        }
+		public PartialViewResult BlogReadAddCommentPartial()
+		{
+			return PartialView();
+		}
 
-        public PartialViewResult FooterLatestBlogsPartial()
-        {
-            return PartialView();
-        }
-
-        public PartialViewResult FooterSignupNewsletterPartial()
-        {
-            return PartialView();
-        }
-
-        public PartialViewResult BlogReadAddCommentPartial()
-        {
-            return PartialView();
-        }
-
-        public PartialViewResult BlogReadRightSignupNewsletterPartial()
-        {
-            return PartialView();
-        }
-
-        public PartialViewResult BlogReadRightSimilarBlogsPartial()
-        {
-            return PartialView();
-        }
-
-        public PartialViewResult BlogReadRightLatestPartial()
-        {
-            return PartialView();
-        }
-    }
+		public PartialViewResult BlogReadRightSignupNewsletterPartial()
+		{
+			return PartialView();
+		}
+	}
 }
