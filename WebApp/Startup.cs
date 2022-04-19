c3babc92fe29255093ae5f0f1e04fa27935c609c
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-20 01:25:40 +0300

    Published

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -80,7 +80,7 @@ namespace WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }
