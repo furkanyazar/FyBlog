912da5440fd0babdd1f87b500cc3cc8dfffd04ed
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-21 23:13:35 +0300

    Add dal, service and manager for notificaiton and notification type

---
+++ Business/Concrete/NotificationManager.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class NotificationManager : EntityManager<Notification, INotificationDal>, INotificationService
+    {
+        public NotificationManager(INotificationDal tdal) : base(tdal)
+        {
+        }
+    }
+}
---
+++ Business/Concrete/NotificationTypeManager.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class NotificationTypeManager : EntityManager<NotificationType, INotificationTypeDal>, INotificationTypeService
+    {
+        public NotificationTypeManager(INotificationTypeDal tdal) : base(tdal)
+        {
+        }
+    }
+}
