1da3eaef4ffe23b38fb90e6ffdbe0cf813576f01
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-22 17:48:11 +0300

    Add dal, service and manager for message

---
+++ Business/Concrete/MessageManager.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class MessageManager : EntityManager<Message, IMessageDal>, IMessageService
+    {
+        public MessageManager(IMessageDal tdal) : base(tdal)
+        {
+        }
+    }
+}
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -6,39 +6,42 @@ using DataAccess.Concrete.EntityFramework;
 
 namespace Business.DependencyResolvers.Autofac
 {
-	public class AutofacBusinessModule : Module
-	{
-		protected override void Load(ContainerBuilder builder)
-		{
-			builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
-			builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
 
-			builder.RegisterType<BlogManager>().As<IBlogService>().SingleInstance();
-			builder.RegisterType<EfBlogDal>().As<IBlogDal>().SingleInstance();
+            builder.RegisterType<BlogManager>().As<IBlogService>().SingleInstance();
+            builder.RegisterType<EfBlogDal>().As<IBlogDal>().SingleInstance();
 
-			builder.RegisterType<CommentManager>().As<ICommentService>().SingleInstance();
-			builder.RegisterType<EfCommentDal>().As<ICommentDal>().SingleInstance();
+            builder.RegisterType<CommentManager>().As<ICommentService>().SingleInstance();
+            builder.RegisterType<EfCommentDal>().As<ICommentDal>().SingleInstance();
 
-			builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
-			builder.RegisterType<EfUserdal>().As<IUserDal>().SingleInstance();
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserdal>().As<IUserDal>().SingleInstance();
 
-			builder.RegisterType<WriterManager>().As<IWriterService>().SingleInstance();
-			builder.RegisterType<EfWriterDal>().As<IWriterDal>().SingleInstance();
+            builder.RegisterType<WriterManager>().As<IWriterService>().SingleInstance();
+            builder.RegisterType<EfWriterDal>().As<IWriterDal>().SingleInstance();
 
-			builder.RegisterType<NewsletterManager>().As<INewsletterService>().SingleInstance();
-			builder.RegisterType<EfNewsletterDal>().As<INewsletterDal>().SingleInstance();
+            builder.RegisterType<NewsletterManager>().As<INewsletterService>().SingleInstance();
+            builder.RegisterType<EfNewsletterDal>().As<INewsletterDal>().SingleInstance();
 
-			builder.RegisterType<AboutManager>().As<IAboutService>().SingleInstance();
-			builder.RegisterType<EfAboutDal>().As<IAboutDal>().SingleInstance();
+            builder.RegisterType<AboutManager>().As<IAboutService>().SingleInstance();
+            builder.RegisterType<EfAboutDal>().As<IAboutDal>().SingleInstance();
 
-			builder.RegisterType<ContactManager>().As<IContactService>().SingleInstance();
-			builder.RegisterType<EfContactDal>().As<IContactDal>().SingleInstance();
+            builder.RegisterType<ContactManager>().As<IContactService>().SingleInstance();
+            builder.RegisterType<EfContactDal>().As<IContactDal>().SingleInstance();
 
-			builder.RegisterType<NotificationManager>().As<INotificationService>().SingleInstance();
-			builder.RegisterType<EfNotificationDal>().As<INotificationDal>().SingleInstance();
+            builder.RegisterType<NotificationManager>().As<INotificationService>().SingleInstance();
+            builder.RegisterType<EfNotificationDal>().As<INotificationDal>().SingleInstance();
 
-			builder.RegisterType<NotificationTypeManager>().As<INotificationTypeService>().SingleInstance();
-			builder.RegisterType<EfNotificationTypeDal>().As<INotificationTypeDal>().SingleInstance();
-		}
-	}
+            builder.RegisterType<NotificationTypeManager>().As<INotificationTypeService>().SingleInstance();
+            builder.RegisterType<EfNotificationTypeDal>().As<INotificationTypeDal>().SingleInstance();
+
+            builder.RegisterType<MessageManager>().As<IMessageService>().SingleInstance();
+            builder.RegisterType<EfMessageDal>().As<IMessageDal>().SingleInstance();
+        }
+    }
 }
