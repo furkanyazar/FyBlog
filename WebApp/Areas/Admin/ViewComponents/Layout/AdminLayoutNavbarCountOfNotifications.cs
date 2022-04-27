aba05b417888a4657e7c6ae10642d0a5337829ce
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-27 23:49:55 +0300

    Add notifications to navbar on admin panel

---
+++ WebApp/Areas/Admin/ViewComponents/Layout/AdminLayoutNavbarCountOfNotifications.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Areas.Admin.ViewComponents.Layout
+{
+    public class AdminLayoutNavbarCountOfNotifications : ViewComponent
+    {
+        private INotificationService _notificationService;
+
+        public AdminLayoutNavbarCountOfNotifications(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _notificationService.GetLatestsByCount().Count;
+
+            return View(result);
+        }
+    }
+}
---
+++ WebApp/Areas/Admin/ViewComponents/Layout/AdminLayoutNavbarNotifications.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Areas.Admin.ViewComponents.Layout
+{
+    public class AdminLayoutNavbarNotifications : ViewComponent
+    {
+        private INotificationService _notificationService;
+
+        public AdminLayoutNavbarNotifications(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _notificationService.GetLatestsByCount();
+
+            return View(result);
+        }
+    }
+}
